60f53a7eec259c1ad6db9fc8ffe4e2a69b005d94
gahitchi <avemodz@gmail.com>
2024-10-02 12:58:39 +0200

    cmd test

--- lightBulb/obj/Debug/net8.0-windows/lightBulb.AssemblyInfo.cs
+++ lightBulb/obj/Debug/net8.0-windows/lightBulb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lightBulb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be16732ce9b5bb42d6c8993979e79334821f3bba")]
 [assembly: System.Reflection.AssemblyProductAttribute("lightBulb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lightBulb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
